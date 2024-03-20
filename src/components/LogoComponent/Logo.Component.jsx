348a06a23857886694269b1ed019a5c69c6c1ea6
Flávia Reis <flaviarster@gmail.com>
2024-03-20 15:03:21 -0300

    Update Logo.Component.jsx

--- src/components/LogoComponent/Logo.Component.jsx
+++ src/components/LogoComponent/Logo.Component.jsx
@@ -2,7 +2,7 @@ import logo from '../../assets/logo.svg'
 
 export const LogoComponent = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center pt-12 pb-[54px] md:pb-16 xl:pt-20 xl:pb-[60px]'>
         <img src={ logo } alt="Logo" />
     </div>
   )
